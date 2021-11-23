f490ee220d00336e0a04293570737851d7aa3ade
Fatih <68196068+fatihozoglu@users.noreply.github.com>
2021-11-23 12:12:57 +0300

    Create initial project structure

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,9 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div id="nav"><router-link to="/">Home</router-link> |</div>
+    <router-view />
   </div>
 </template>
-
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
 
 <style>
 #app {
@@ -23,6 +12,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from "vue";
+import store from "./store";
 import App from "./App.vue";
+import router from "./router";
 
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
+  store,
   render: (h) => h(App),
 }).$mount("#app");
