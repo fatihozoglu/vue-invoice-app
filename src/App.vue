910c1db920b7c0eb9e1da510cac122c0c195571a
Fatih <68196068+fatihozoglu@users.noreply.github.com>
2021-11-23 14:06:45 +0300

    Add HeaderComp.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,33 @@
 <template>
   <div id="app">
-    <div id="nav"><router-link to="/">Home</router-link> |</div>
-    <router-view />
+    <HeaderComp />
+    <router-view class="router-view" />
   </div>
 </template>
 
+<script>
+import HeaderComp from "./components/HeaderComp.vue";
+
+export default {
+  name: "App",
+  components: {
+    HeaderComp,
+  },
+};
+</script>
+
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  background-color: #2b2f4d;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  background-color: #141624;
+  max-width: 1280px;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="header">
+    <div class="logo-container">
+      <a
+        href="https://www.frontendmentor.io/challenges/invoice-app-i7KaLTQjl"
+        target="_blank"
+      >
+        <div class="logo">
+          <div class="logo-rect"></div>
+        </div>
+      </a>
+    </div>
+    <div class="user-info">
+      <a href="https://github.com/fatihozoglu" target="_blank">
+        <img
+          class="user-photo"
+          width="40px"
+          height="40px"
+          :src="require('../assets/fatih-ozoglu-logo.jpg')"
+          alt="profile photo"
+        />
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderComp",
+  props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header {
+  height: 100vh;
+  width: 103px;
+  background-color: #1e2139;
+  border-top-right-radius: 20px;
+  border-bottom-right-radius: 20px;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.logo-container {
+  width: 100%;
+  height: 103px;
+  background-color: #7b5cfa;
+  border-bottom-right-radius: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.logo {
+  width: 40px;
+  height: 40px;
+  position: relative;
+  background-color: white;
+  border-radius: 50%;
+}
+.logo-rect {
+  background: #7b5cfa;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  clip-path: polygon(80% 0, 20% 0, 50% 50%);
+}
+.user-info {
+  width: 100%;
+  height: 90px;
+  border-top: 1px solid rgb(73, 79, 110);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.user-photo {
+  border-radius: 50%;
+}
+</style>
