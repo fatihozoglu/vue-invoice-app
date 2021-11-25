e4f1686f2742d82c99b6a53bf4756b4182a4e56e
Fatih <68196068+fatihozoglu@users.noreply.github.com>
2021-11-25 19:30:53 +0300

    Fix layout issues

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -32,26 +32,44 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .home {
-  padding: 70px 215px;
-  min-height: 100vh;
+  padding: 50px 215px;
+  height: 100vh;
   width: 100vw;
 }
+.invoices-container {
+  height: 90%;
+  overflow-y: scroll;
+  padding-right: 10px;
+}
 .header {
-  margin-bottom: 50px;
+  margin-bottom: 30px;
+}
+
+::-webkit-scrollbar {
+  width: 8px;
+}
+/* Track */
+::-webkit-scrollbar-track {
+  background: #141624;
+}
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #252946;
+  border-radius: 20px;
 }
 
 @media screen and (max-width: 1024px) {
   .home {
-    padding: 100px 120px;
+    padding: 100px 120px 20px 120px;
   }
   .header {
-    margin-bottom: 30px;
+    margin-bottom: 20px;
   }
 }
 
 @media screen and (max-width: 750px) {
   .home {
-    padding: 100px 20px;
+    padding: 100px 20px 20px 20px;
   }
 }
 </style>
